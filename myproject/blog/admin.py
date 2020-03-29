74f0070c9ea386848df1f5bfba8be4d765a3311c
miksart <mrtaremi78@gmail.com>
2020-03-29 19:22:18 +0430

    create models and set setting admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleClass(admin.ModelAdmin):
+    list_display = (('title'),('slug'),('publish'),('status'),)
+    prepopulated_fields = {'slug':('title', )}
+    list_filter = ('status', 'publish', )
+    ordering = ['publish', ]
+    search_fields = ('title', 'description', )
+
+admin.site.register(Article, ArticleClass)
+
