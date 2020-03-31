e8e100bc26839f0349d3e5e56f7c1a2bcc924197
miksart <mrtaremi78@gmail.com>
2020-03-31 10:47:51 +0430

    fix in slug url for 404

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
 def home(request):
     context = {
         'title': 'main',
-        'articles': Article.objects.all()
+        'articles': Article.objects.filter(status='P')
     }
     return render(request, 'blog/index.html', context)
 
 
 def detail(request, slug):
     context = {
-        'article': Article.objects.get(slug=slug),
+        'article': get_object_or_404(Article, slug=slug, status='P'),
 
     }
     return render(request, 'blog/detail.html', context)
